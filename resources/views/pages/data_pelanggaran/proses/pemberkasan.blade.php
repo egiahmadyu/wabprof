ad5ffb5fa5a2aa7feac2fa002290add0dfadd6f6
jorgifatwa <jorgifatwaambia@gmail.com>
2023-04-11 19:00:57 +0700

    pemberkasan finish

--- resources/views/pages/data_pelanggaran/proses/pemberkasan.blade.php
+++ resources/views/pages/data_pelanggaran/proses/pemberkasan.blade.php
@@ -211,6 +211,7 @@
     @if (isset($kasus) & ($kasus->status_id === 6))
         <div class="row mt-4">
             <div class="col-lg-12">
+            @if (isset($administrasi_sidang) && isset($penyerahan) && isset($perbaikan) && isset($permohonan))
                 <form action="/data-kasus/update" method="post">
                     @csrf
                     <input type="text" class="form-control" value="{{ $kasus->id }}" hidden name="kasus_id">
@@ -220,7 +221,13 @@
                         Lanjutkan ke proses Sidang KEPP
                     </button>
                 </form>
+            @else
+                <button class="btn btn-success disabled">
+                        Lanjutkan ke proses Sidang KEPP
+                    </button>
+            @endif
             </div>
+
         </div>
     @endif
 </div>
@@ -230,31 +237,31 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Administrasi Sidang</h5>
-                <button type="button" class="btn-close btn-tutup" form="form-admistrasi-sidang" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close btn-tutup" form="form-administrasi-sidang" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form action="/administrasi-sidang" method="post" id="form-admistrasi-sidang">
+            <form action="/administrasi-sidang" method="post" id="form-administrasi-sidang">
                 @csrf
                 <div class="modal-body">
                     <input type="hidden" name="data_pelanggar_id" value="{{ $kasus->id }}">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Tanggal</label>
-                        <input type="date" class="form-control" name="tanggal" aria-describedby="emailHelp">
+                        <input type="date" class="form-control" name="tanggal" aria-describedby="emailHelp" id="tanggal">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Jam</label>
-                        <input type="time" class="form-control" name="jam">
+                        <input type="time" class="form-control" name="jam" id="jam">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Tempat</label>
-                        <input type="text" class="form-control" name="tempat" placeholder="Tempat">
+                        <input type="text" class="form-control" name="tempat" placeholder="Tempat" id="tempat">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Pakaian</label>
-                        <input type="text" class="form-control" name="pakaian" placeholder="Pakaian">
+                        <input type="text" class="form-control" name="pakaian" placeholder="Pakaian" id="pakaian">
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary btn-tutup" form="form-admistrasi-sidang" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary btn-tutup" form="form-administrasi-sidang" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary btn-generate" modal="modal_administrasi_sidang">Generate</button>
                 </div>
             </form>
@@ -275,16 +282,16 @@
                     <input type="hidden" name="data_pelanggar_id" value="{{ $kasus->id }}">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Nomor LP-A</label>
-                        <input type="text" class="form-control" name="nomor" aria-describedby="emailHelp" placeholder="Nomor LP-A">
+                        <input type="text" class="form-control" name="nomor" id="nomor" aria-describedby="emailHelp" placeholder="Nomor LP-A">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Tanggal LP-A</label>
-                        <input type="date" class="form-control" name="tanggal">
+                        <input type="date" class="form-control" name="tanggal" id="tanggal">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Terduga</label>
                         @if (isset($perbaikan))
-                            <select name="bp3kepp_id" id="bp3kepp_id" class="form-control">
+                            <select name="bp3kepp_id" id="bp3kepp_id" class="form-control" required>
                                 <option value="">Pilih Terudga</option>
                                 @foreach($perbaikan_data as $perbaikan)
                                     <option value="{{ $perbaikan->id }}">{{ $perbaikan->nama }}</option>
@@ -294,7 +301,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Pasal Yang Disangkakan</label>
-                        <input type="text" class="form-control" name="pasal" placeholder="Pasal">
+                        <input type="text" class="form-control" name="pasal" id="pasal" placeholder="Pasal">
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -320,11 +327,11 @@
                     <input type="hidden" name="data_pelanggar_id" value="{{ $kasus->id }}">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Tanggal BP3KEPP</label>
-                        <input type="date" class="form-control" name="tanggal" aria-describedby="emailHelp">
+                        <input type="date" class="form-control" name="tanggal" aria-describedby="emailHelp" id="tanggal">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Nomor BP3KEPP</label>
-                        <input type="text" class="form-control" name="nomor" placeholder="Nomor BP3KEPP">
+                        <input type="text" class="form-control" name="nomor" placeholder="Nomor BP3KEPP" id="nomor">
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -386,7 +393,7 @@
                         <hr>
                     </div>
                     <div class="row mb-3" class="d-flex justify-content-end">
-                        <a href="#" onclick="tambahTerduga()"> <i class="far fa-plus-square"></i>
+                        <a href="#" id="tambah" counter="0"> <i class="far fa-plus-square"></i>
                             Terduga </a>
                     </div>
                 </div>
@@ -400,83 +407,243 @@
 </div>
 
 <script>
-    // $(document).ready(function() {
-    //     getNextData()
-    // });
-
-    // function getNextData() {
-    //     console.log($('#test_sprin').val())
-    //     if ($('#test_sprin').val() == 'done') {
-
-    //         $.ajax({
-    //             url: `/pulbaket/view/next-data/` + $('#kasus_id').val(),
-    //             method: "get"
-    //         }).done(function(data) {
-    //             $('.loader-view').css("display", "none");
-    //             $("#viewNext").html(data)
-    //         });
-    //     }
-    // }
-
-    function tambahTerduga() {
+     $(document).ready(function() {
+        $('#form-administrasi-sidang').validate({
+            rules: {
+                tanggal : {
+                    required: true,
+                },
+                jam : {
+                    required: true,
+                },
+                tempat : {
+                    required: true,
+                },
+                pakaian : {
+                    required: true,
+                },
+            },
+            messages : {
+                tanggal: "Silahkan isi tanggal!",
+                jam: "Silahkan isi jam!",
+                tempat: "Silahkan isi tempat!",
+                pakaian: "Silahkan isi pakaian!",
+            },
+            errorElement : 'label',
+            errorClass: 'text-danger',
+            errorPlacement: function(error, element) {
+                error.insertAfter(element);
+            },
+            success: function(label,element) {
+                label.parent().removeClass('error');
+                label.remove(); 
+            },
+            submitHandler: function (form) { // for demo
+                form.submit();
+                var kasus_id = $('#kasus_id').val();
+                var id = $('#status_id').val();
+                $('#modal_administrasi_sidang').modal('hide');
+                $('.loader-view').show();
+                $('#viewProses').hide();
+                setTimeout(function() {
+                    $.ajax({
+                        type: 'get',
+                        url: `/data-kasus/view/${kasus_id}/${id}`,
+                        success: function(data) {
+                            $('#viewProses').html(data);
+                            $('.loader-view').hide();
+                            $('#viewProses').show();
+                        }
+                    });
+                }, 3000);
+            }
+        });
+        $('#form-penyerahan').validate({
+            rules: {
+                tanggal : {
+                    required: true,
+                },
+                nomor : {
+                    required: true,
+                },
+            },
+            messages : {
+                tanggal: "Silahkan isi tanggal!",
+                nomor: "Silahkan isi nomor!",
+            },
+            errorElement : 'label',
+            errorClass: 'text-danger',
+            errorPlacement: function(error, element) {
+                error.insertAfter(element);
+            },
+            success: function(label,element) {
+                label.parent().removeClass('error');
+                label.remove(); 
+            },
+            submitHandler: function (form) { // for demo
+                form.submit();
+                var kasus_id = $('#kasus_id').val();
+                var id = $('#status_id').val();
+                $('#modal_nota_dinas_penyerahan').modal('hide');
+                $('.loader-view').show();
+                $('#viewProses').hide();
+                setTimeout(function() {
+                    $.ajax({
+                        type: 'get',
+                        url: `/data-kasus/view/${kasus_id}/${id}`,
+                        success: function(data) {
+                            $('#viewProses').html(data);
+                            $('.loader-view').hide();
+                            $('#viewProses').show();
+                        }
+                    });
+                }, 3000);
+            }
+        });
+        $('#form-perbaikan').validate({
+            rules: {
+                "tanggal[]":'required',
+                'nomor[]' :'required',
+                'nrp[]' :'required',
+                'nama[]' :'required',
+                'pangkat[]' :'required',
+                'jabatan[]' :'required',
+                'kesatuan[]' :'required',
+            },
+            messages : {
+                'tanggal[]': "Silahkan isi tanggal!",
+                'nomor[]': "Silahkan isi nomor!",
+                'nrp[]': "Silahkan isi nrp!",
+                'nama[]': "Silahkan isi nama!",
+                'pangkat[]': "Silahkan isi pangkat!",
+                'jabatan[]': "Silahkan isi jabatan!",
+                'kesatuan[]': "Silahkan isi kesatuan!",
+            },
+            errorElement : 'label',
+            errorClass: 'text-danger',
+            errorPlacement: function(error, element) {
+                error.insertAfter(element);
+            },
+            success: function(label,element) {
+                label.parent().removeClass('error');
+                label.remove(); 
+            },
+            submitHandler: function (form) { // for demo
+                form.submit();
+                var kasus_id = $('#kasus_id').val();
+                var id = $('#status_id').val();
+                $('#modal_nota_dinas_perbaikan').modal('hide');
+                $('.loader-view').show();
+                $('#viewProses').hide();
+                setTimeout(function() {
+                    $.ajax({
+                        type: 'get',
+                        url: `/data-kasus/view/${kasus_id}/${id}`,
+                        success: function(data) {
+                            $('#viewProses').html(data);
+                            $('.loader-view').hide();
+                            $('#viewProses').show();
+                        }
+                    });
+                }, 3000);
+            }
+        });
+        $('#form-permohonan').validate({
+            rules: {
+                tanggal : {
+                    required: true,
+                },
+                nomor : {
+                    required: true,
+                },
+                pasal : {
+                    required: true,
+                },
+                bp3kepp_id : {
+                    required: true,
+                },
+            },
+            messages : {
+                tanggal: "Silahkan isi tanggal!",
+                nomor: "Silahkan isi nomor!",
+                pasal: "Silahkan isi pasal!",
+                bp3kepp_id: "Silahkan pilih terduga!",
+            },
+            errorElement : 'label',
+            errorClass: 'text-danger',
+            errorPlacement: function(error, element) {
+                error.insertAfter(element);
+            },
+            success: function(label,element) {
+                label.parent().removeClass('error');
+                label.remove(); 
+            },
+            submitHandler: function (form) { // for demo
+                form.submit();
+                var kasus_id = $('#kasus_id').val();
+                var id = $('#status_id').val();
+                $('#modal_permohonan').modal('hide');
+                $('.loader-view').show();
+                $('#viewProses').hide();
+                setTimeout(function() {
+                    $.ajax({
+                        type: 'get',
+                        url: `/data-kasus/view/${kasus_id}/${id}`,
+                        success: function(data) {
+                            $('#viewProses').html(data);
+                            $('.loader-view').hide();
+                            $('#viewProses').show();
+                        }
+                    });
+                }, 3000);
+            }
+        });
+    });
+    $('#tambah').on('click', function () {
+       var counter = $(this).attr('counter');
+       var counter = parseInt(counter)+1;
+       tambahTerduga(counter);
+        $(this).attr('counter', counter);
+    });
+    function tambahTerduga(counter) {
         let inHtml =
             `<div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="exampleInputEmail1" class="form-label">Tanggal BP3KEPP</label>
-                    <input type="date" class="form-control" name="tanggal[]" aria-describedby="emailHelp">
+                    <input type="date" class="form-control" name="tanggal[]" id="tanggal_${counter}" aria-describedby="emailHelp">
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="exampleInputPassword1" class="form-label">Nomor BP3KEPP</label>
-                    <input type="text" class="form-control" name="nomor[]" placeholder="Nomor BP3KEPP">
+                    <input type="text" class="form-control" name="nomor[]" id="nomor_${counter}" placeholder="Nomor BP3KEPP">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="exampleInputPassword1" class="form-label">NRP Terduga</label>
-                    <input type="text" class="form-control" name="nrp[]" placeholder="NRP Terduga">
+                    <input type="text" class="form-control" name="nrp[]" id="nrp_${counter}" placeholder="NRP Terduga">
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="exampleInputPassword1" class="form-label">Nama Terduga</label>
-                    <input type="text" class="form-control" name="nama[]" placeholder="Nama Terduga">
+                    <input type="text" class="form-control" name="nama[]" id="nama_${counter}" placeholder="Nama Terduga">
                 </div>   
             </div>
             <div class="row">
                 <div class="col-md-4 mb-3">
                     <label for="exampleInputPassword1" class="form-label">Pangkat Terduga</label>
-                    <input type="text" class="form-control" name="pangkat[]" placeholder="Pangkat Terduga">
+                    <input type="text" class="form-control" name="pangkat[]" id="pangkat_${counter}" placeholder="Pangkat Terduga">
                 </div>
                 <div class="col-md-4 mb-3">
                     <label for="exampleInputPassword1" class="form-label">Jabatan Terduga</label>
-                    <input type="text" class="form-control" name="jabatan[]" placeholder="Jabatan Terduga">
+                    <input type="text" class="form-control" name="jabatan[]" id="jabatan_${counter}" placeholder="Jabatan Terduga">
                 </div>
                 <div class="col-md-4 mb-3">
                     <label for="exampleInputPassword1" class="form-label">Kesatuan Terduga</label>
-                    <input type="text" class="form-control" name="kesatuan[]" placeholder="Kesatuan Terduga">
+                    <input type="text" class="form-control" name="kesatuan[]" id="kesatuan_${counter}" placeholder="Kesatuan Terduga">
                 </div>
             </div>
             <hr>`;
         $('#form_input_terduga').append(inHtml);
     }
-    $('.btn-generate').on('click', function () {
-        var modal = $(this).attr('modal');
-        var kasus_id = $('#kasus_id').val();
-        var id = $('#status_id').val();
-        $('#'+modal).modal('hide');
-        $('.loader-view').show();
-        $('#viewProses').hide();
-        setTimeout(function() {
-            $.ajax({
-                type: 'get',
-                url: `/data-kasus/view/${kasus_id}/${id}`,
-                success: function(data) {
-                    $('#viewProses').html(data);
-                    $('.loader-view').hide();
-                    $('#viewProses').show();
-                }
-            });
-        }, 3000);
-    });
-
     $('.btn-tutup').on('click', function () {
         var form = $(this).attr('form');
         $('#'+form).find("input[type=text], input[type=time], input[type=date], textarea").val("");
